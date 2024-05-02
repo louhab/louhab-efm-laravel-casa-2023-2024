a41cd027e84886630a16dbb1379daa60d3c08f92
louhab <louhab.abderazzak@gmail.com>
2024-05-02 15:08:28 +0200

    adding edditing for version2

---
+++ v1/database/migrations/2024_05_02_130225_create_commandes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('repas_id')->unsigned();
+            $table->string('intitule');
+            $table->string('description');
+
+
+
+            $table->unsignedInteger('categorie_id');
+            $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
---
+++ v1/database/migrations/2024_05_02_130232_create_repas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('intitule');
+            $table->string('description');
+
+            $table->unsignedInteger('categorie_id');
+            $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('cascade');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('repas');
+    }
+}
