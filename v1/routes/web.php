05b578ddb5265beb0a7f65c83ca1943e9d0e16f1
louhab <louhab.abderazzak@gmail.com>
2024-05-02 17:11:36 +0200

    finiching the correction

--- v1/routes/web.php
+++ v1/routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\RepasController;
 
+Route::middleware('auth')->group(function () {
+
 Route::get('/', [RepasController::class, "index"])->name("repas.index");
 Route::delete('/repas/{id}', [RepasController::class, "destroy"])->name("repas.destroy");
 Route::post('/repas', [RepasController::class, "AddRepas"])->name("repas.store");
 Route::get('/repas', [RepasController::class, "create"])->name("repas.create");
+});
