bce0707c3668c2a47fa61911c93843ad092707db
louhab <louhab.abderazzak@gmail.com>
2024-05-02 13:41:54 +0200

    the fisrt commit

--- v2/routes/web.php
+++ v2/routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
 
-Route::get('/', [HotelController::class, "index"])->name("hotels.index");
-Route::delete('/hotels/{id}', [HotelController::class, "destroy"])->name("hotels.destroy");
-Route::POST('/hotels', [HotelController::class, "AjouterHotel"])->name("hotels.store");
-Route::GET('/hotels', [HotelController::class, "create"])->name("hotels.create");
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HotelController::class, "index"])->name("hotels.index");
+    Route::delete('/hotels/{id}', [HotelController::class, "destroy"])->name("hotels.destroy");
+    Route::post('/hotels', [HotelController::class, "AjouterHotel"])->name("hotels.store");
+    Route::get('/hotels', [HotelController::class, "create"])->name("hotels.create");
+});
 
 
